1c871bd3c47026d3464bc8bab20c98461a496e56
Mayur Varma <mayurvarma14@gmail.com>
2019-11-19 04:50:07 +0530

    Added auth redux state

--- client/src/components/GoogleAuth.js
+++ client/src/components/GoogleAuth.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
+import {signIn,signOut} from '../actions'
 class GoogleAuth extends Component {
-    state={isSignedIn:null}
 
     componentDidMount() {
         window.gapi.load('client:auth2',() => {
@@ -11,21 +12,25 @@ class GoogleAuth extends Component {
             })
                 .then(() => {
                     this.auth= window.gapi.auth2.getAuthInstance()
-                    this.setState({isSignedIn:this.auth.isSignedIn.get()})
+                    this.onAuthChange(this.auth.isSignedIn.get())
                     this.auth.isSignedIn.listen(this.onAuthChange)
                 })
         })
     }
 
-    onAuthChange=() => {
-        this.setState({isSignedIn:this.auth.isSignedIn.get()})
+    onAuthChange=(isSignedIn) => {
+        if (isSignedIn) {
+            this.props.signIn(this.auth.currentUser.get().getId())
+        } else {
+            this.props.signOut()
+        }  
     }
 
     renderAuthButton(){
-        if (this.state.isSignedIn===null) {
+        if (this.props.isSignedIn===null) {
             return null
         }
-        else if (this.state.isSignedIn) {
+        else if (this.props.isSignedIn) {
             return (
                 <button className="ui red google button" onClick={this.auth.signOut}>
                     <i className="google icon"></i>
@@ -48,5 +53,7 @@ class GoogleAuth extends Component {
         )
     }
 }
-
-export default GoogleAuth
+const mapStateToProps=(state) => {
+return {isSignedIn:state.auth.isSignedIn}
+}
+export default connect(mapStateToProps,{signIn,signOut})(GoogleAuth)
---
+++ client/src/reducers/authReducer.js
@@ -0,0 +1,19 @@
+import {SIGN_IN,SIGN_OUT} from '../actions/types'
+
+const initialState = {
+isSignedIn:null,
+userId:null
+}
+
+export default (state = initialState, { type, payload }) => {
+    switch (type) {
+
+    case SIGN_IN:
+        return { ...state, isSignedIn:true,userId:payload }
+    case SIGN_OUT:
+        return { ...state, isSignedIn:false,userId:null }
+
+    default:
+        return state
+    }
+}
